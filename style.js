b03efa30c124439b29d50b9be88de497960da9a2
Ramin jamal <131708198+jamalramin@users.noreply.github.com>
2023-07-14 23:45:12 +0430

    Add files via upload

---
+++ style.js
@@ -0,0 +1,54 @@
+function scr(){
+    document.getElementById("header").style.backgroundColor = "gray";
+}
+function Consistency(){
+    document.querySelector("section").style.backgroundColor = "red";
+    var title = document.getElementById("title");
+    var para = document.getElementById("para");
+    var img = document.getElementById("image-char");
+
+    title.innerHTML = "Consistency";
+    para.innerHTML = "We established our trust with our clients which led us to better customer services and great customer satisfaction. Our team has been researching and analyzing our products to ensure we provide you with the results in an efficient manner. At RichLabz we maintain all the ideas, implementation, and action updated with our clients in all possible sources. This results in our clients becoming our loyal customers.";
+    img.src = "./../images/constistency.webp";
+}
+
+function quality(){
+    var title = document.getElementById("title");
+    var img = document.getElementById("image-char");
+    var para = document.getElementById("para");
+
+    title.innerHTML = "Quality";
+    para.innerHTML = "RichLabz believes in providing quality IT solutions to small and medium-sized businesses that empower them in the competitive era. When it comes to design thinking and marketing solutions we stand out the best from the rest. Our services range from developing websites, online stores, app design & development, digital marketing & social media marketing.";
+    img.src = "./../images/quailty.webp";
+}
+
+function Commitment(){
+    var title = document.getElementById("title");
+    var img = document.getElementById("image-char");
+    var para = document.getElementById("para");
+
+    title.innerHTML = "Commitment";
+    para.innerHTML = "We are committed to delivering the best possible service to our clients and their customers. We have a strong belief that quality and efficiency are key to success. In RichLabz, we strive to deliver high-quality services at competitive prices. This is why we offer a wide range of IT services, including website designing and development, application designing and development, digital marketing, and social media marketing.";
+    img.src = "./../images/commement.webp";
+}
+
+function Reliability(){
+    var title = document.getElementById("title");
+    var para = document.getElementById("para");
+    var img = document.getElementById("image-char");
+
+    title.innerHTML = "Reliability";
+    para.innerHTML = "We are a reliable website and app development service for your business. Our team of developers is well-versed in the latest technologies, which help you build a successful business. We have 6 years of experience in taking your digital idea to market. We provide you with all the necessary tools that will help you advertise using our digital and social media marketing solutions.";
+    img.src = "./../images/rpliability.webp";
+}
+
+function Sincerity(){
+    var title = document.getElementById("title");
+    var img = document.getElementById("image-char");
+    var para = document.getElementById("para");
+
+
+    title.innerHTML = "Sincerity";
+    para.innerHTML = "We at RichLabz have the most fundamental and important relationship with the clients. We always ensure to care, understand and implement the ideas of our clients to turn their business dreams into reality. We thrive to accomplish the task as per the committed turnaround time. We ensure to implement what good is for your business in terms of altering or articulating your ideas within the given period of time.";
+    img.src = "./../images/Sincerity.jpg";
+}
